fe41e5bb94f0e7f777229f903df45be247d45ea3
GrantRoss-Tenki <97212730+GrantRoss-Tenki@users.noreply.github.com>
2022-01-11 13:52:12 -0800

    Update Graphing_Summaries.py
    First start in organization

--- Graphing_Summaries.py
+++ Graphing_Summaries.py
@@ -5,4 +5,9 @@ y = np.arange(0,5)
 x = np.arange(5,10)
 print('this is the y value',y)
 
-plt show()
+# Is this a personal or work computer
+
+Computer = 'work'
+
+if Computer == 'Personal':
+    datafile_path ="C:/Users/gvros/Desktop/Oregon State Masters/Work/OSU, CSC, CQC Project files/1H/1N_Summary_Day_rank_1_exact.csv"

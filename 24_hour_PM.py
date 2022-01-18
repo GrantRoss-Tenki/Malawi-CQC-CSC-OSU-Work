13efc7eaf7966e45e12b71fb2accb78aa2116853
GrantRoss-Tenki <97212730+GrantRoss-Tenki@users.noreply.github.com>
2022-01-17 16:43:48 -0800

    Create 24_hour_PM.py

---
+++ 24_hour_PM.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 17 16:28:26 2022
+
+@author: rossgra
+"""
+import pandas as pd
+import numpy as np
+import csv
+import statistics as stat
+
+
+# THis file is for gathering 24 hour averages 

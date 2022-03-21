2865fea24b66075ab7912bdcaa67ea3e4078f0cd
GrantRoss-Tenki <97212730+GrantRoss-Tenki@users.noreply.github.com>
2022-03-20 21:24:55 -0700

    Update MJ_per_SAE_STAT_GRAPH.py

--- MJ_per_SAE_STAT_GRAPH.py
+++ MJ_per_SAE_STAT_GRAPH.py
@@ -20,10 +20,10 @@ Level_of_confidence = 0.05
 No_hood_MJ = pd.read_csv(No_hood_MJ_path)
 Hood_MJ = pd.read_csv(Hood_MJ_Path)
 #C:\Users\rossgra\Box\Classes\Software Dev C:\Users\rossgra\Box\OSU, CSC, CQC Project files
-HH_1N = No_hood_MJ.iloc[:, 0]
-HH_2N = No_hood_MJ.iloc[:, 11]
-HH_3N = No_hood_MJ.iloc[:, 22]
-HH_4N = No_hood_MJ.iloc[:, 33]
+HH_1N = [x for x in No_hood_MJ.iloc[:, 0] if x != -1]
+HH_2N = [x for x in  No_hood_MJ.iloc[:, 11] if x != -1]
+HH_3N = [x for x in  No_hood_MJ.iloc[:, 22] if x != -1]
+HH_4N = [x for x in  No_hood_MJ.iloc[:, 33] if x != -1]
 
 HH_1H = [x for x in Hood_MJ.iloc[:, 0] if x != -1]
 HH_2H = [x for x in Hood_MJ.iloc[:, 11] if x != -1]
@@ -67,12 +67,12 @@ Avg_Fuel_3H = [x for x in Hood_MJ.iloc[:, 24] if x != -1]
 
 Phase_1N_day_count = [x for x in No_hood_MJ.iloc[:, 1] if x != -1]
 Phase_2N_day_count = [x for x in No_hood_MJ.iloc[:, 12] if x != -1]
-Phase_3N_day_count = [x for x in No_hood_MJ.iloc[:, 13] if x != -1]
-Phase_4N_day_count = [x for x in No_hood_MJ.iloc[:, 14] if x != -1]
+Phase_3N_day_count = [x for x in No_hood_MJ.iloc[:, 23] if x != -1]
+Phase_4N_day_count = [x for x in No_hood_MJ.iloc[:, 34] if x != -1]
 
 Phase_1H_day_count = [x for x in Hood_MJ.iloc[:, 1] if x != -1]
 Phase_2H_day_count = [x for x in Hood_MJ.iloc[:, 12] if x != -1]
-Phase_3H_day_count = [x for x in Hood_MJ.iloc[:, 13] if x != -1]
+Phase_3H_day_count = [x for x in Hood_MJ.iloc[:, 23] if x != -1]
 
 Filter_1N_day_count = [x for x in No_hood_MJ.iloc[:, 7] if x != -1]
 Filter_2N_day_count = [x for x in No_hood_MJ.iloc[:, 18] if x != -1]
@@ -138,16 +138,18 @@ MJ_Phase_3N_to_1_comon = []
 
 Day_count_MJ_Phase_1N_3N = []
 count_n = 0
+breakme = 0
 for row_1N, hh_1N in enumerate(HH_1N):
-    if hh_1N == str(-1):
+    if hh_1N == (-1) :
         break
     for row_3N, hh_3N in enumerate(HH_3N):
         if hh_1N == hh_3N:
-            print(row_3N,row_1N, hh_1N,hh_3N)
+            
             MJ_Phase_1N_to_3_comon.append(Mj_1N_Phase[row_1N])
             MJ_Phase_3N_to_1_comon.append(Mj_3N_Phase[row_3N])
             Day_count_MJ_Phase_1N_3N.append(Phase_1N_day_count[row_1N] + Phase_3N_day_count[row_3N])
             count_n = count_n + 1
+
             
 N_MJ_Phase_1N_3N = count_n -1
 #for filter 
@@ -161,6 +163,7 @@ for row_1N, hh_1N in enumerate(HH_1N):
         break
     for row_3N, hh_3N in enumerate(HH_3N):
         if hh_1N == hh_3N:
+            
             MJ_filter_1N_to_3_comon.append(Mj_filter_1N_Phase[row_1N])
             MJ_filter_3N_to_1_comon.append(Mj_filter_3N_Phase[row_3N])
             
@@ -282,35 +285,35 @@ if degree_1N_2N < abs(T_stat_1N_2N):
     print('1N and 2N Phase rejects the null', T_stat_1N_2N,'P-value', P_val_1N_2N,'Sample size N', N_MJ_Phase_1N_2N)
 else:
     print('1N and 2N Phase accepts the null', T_stat_1N_2N,'P-value', P_val_1N_2N,'Sample size N', N_MJ_Phase_1N_2N)
-    
+T_sign_1N_2N, P_sign_1N_2N = scipy.stats.wilcoxon(MJ_Phase_1N_to_2_comon, MJ_Phase_2N_to_1_comon)
+
 T_stat_1N_2N_filter, P_val_1N_2N_filter = scipy.stats.ttest_ind(MJ_filter_1N_to_2_comon,MJ_filter_2N_to_1_comon, axis=0, equal_var=True)
 degree_1N_2N_filter = (N_MJ_filter_1N_2N -1) *Level_of_confidence
 if degree_1N_2N_filter < abs(T_stat_1N_2N_filter):
     print('1N and 2N Filter rejects the null', T_stat_1N_2N_filter,'P-value', P_val_1N_2N_filter,'Sample size N', N_MJ_filter_1N_2N)
 else:
     print('1N and 2N Filter accepts the null', T_stat_1N_2N_filter,'P-value', P_val_1N_2N_filter,'Sample size N', N_MJ_filter_1N_2N)
-    
+T_sign_1N_2N_filter, P_sign_1N_2N_filter = scipy.stats.wilcoxon(MJ_filter_1N_to_2_comon, MJ_filter_2N_to_1_comon)
+
+
 T_stat_1N_3N, P_val_1N_3N = scipy.stats.ttest_ind(MJ_Phase_1N_to_3_comon,MJ_Phase_3N_to_1_comon, axis=0, equal_var=True)
 degree_1N_3N = (N_MJ_Phase_1N_3N -1) *Level_of_confidence
 if degree_1N_3N < abs(T_stat_1N_3N):
     print('1N and 3N Phase rejects the null', T_stat_1N_3N,'P-value', P_val_1N_3N,'Sample size N', N_MJ_Phase_1N_3N)
 else:
     print('1N and 3N Phase accepts the null', T_stat_1N_3N,'P-value', P_val_1N_3N,'Sample size N', N_MJ_Phase_1N_3N)
-    
+T_sign_1N_3N, P_sign_1N_3N = scipy.stats.wilcoxon(MJ_Phase_1N_to_3_comon, MJ_Phase_3N_to_1_comon)
+
 T_stat_1N_3N_filter, P_val_1N_3N_filter = scipy.stats.ttest_ind(MJ_filter_1N_to_3_comon,MJ_filter_3N_to_1_comon, axis=0, equal_var=True)
 degree_1N_3N_filter = (N_MJ_filter_1N_3N -1) *Level_of_confidence
 if degree_1N_3N_filter < abs(T_stat_1N_3N_filter):
     print('1N and 3N Filter rejects the null', T_stat_1N_3N_filter,'P-value', P_val_1N_3N_filter,'Sample size N', N_MJ_filter_1N_3N)
 else:
     print('1N and 3N Filter accepts the null', T_stat_1N_3N_filter,'P-value', P_val_1N_3N_filter,'Sample size N', N_MJ_filter_1N_3N)
-    
-    
+T_sign_1N_3N_filter, P_sign_1N_3N_filter = scipy.stats.wilcoxon(MJ_filter_1N_to_3_comon, MJ_filter_1N_to_3_comon)
+
 T_stat_1N_4N, P_val_1N_4N = scipy.stats.ttest_ind(MJ_Phase_1N_to_4_comon,MJ_Phase_4N_to_1_comon, axis=0, equal_var=True)
 degree_1N_4N = (N_MJ_Phase_1N_4N -1) *Level_of_confidence
-
-
-print('length of 1n and 4 n:', len(MJ_Phase_1N_to_4_comon), len(MJ_Phase_4N_to_1_comon) )
-print('Degree of frededom',degree_1N_4N, T_stat_1N_4N )
 if degree_1N_4N < abs(T_stat_1N_4N):
     print('1N and 4N Phase rejects the null', T_stat_1N_4N,'P-value', P_val_1N_4N,'Sample size N', N_MJ_Phase_1N_4N)
     T_sign_1N_4N, P_sign_1N_4N = scipy.stats.wilcoxon(MJ_Phase_1N_to_4_comon, MJ_Phase_4N_to_1_comon)
@@ -318,7 +321,7 @@ if degree_1N_4N < abs(T_stat_1N_4N):
 else:
     print('1N and 4N Phase accepts the null', T_stat_1N_4N,'P-value', P_val_1N_4N,'Sample size N', N_MJ_Phase_1N_4N)
     print('Tried Wilcox Sign Test, Tstat= ',T_sign_1N_4N, 'P value= ',P_sign_1N_4N, 'Degree of freedom = ',degree_1N_4N, 'Accepted if + ',degree_1N_4N - abs(T_sign_1N_4N)  )
-    
+  
     
 T_stat_1N_4N_filter, P_val_1N_4N_filter = scipy.stats.ttest_ind(MJ_filter_1N_to_4_comon,MJ_filter_4N_to_1_comon, axis=0, equal_var=True)
 degree_1N_4N_filter = (N_MJ_filter_1N_4N -1) *Level_of_confidence
@@ -326,7 +329,7 @@ if degree_1N_4N_filter < abs(T_stat_1N_4N_filter):
     print('1N and 4N Filter rejects the null', T_stat_1N_4N_filter,'P-value', P_val_1N_4N_filter,'Sample size N', N_MJ_filter_1N_4N)
 else:
     print('1N and 4N Filter accepts the null', T_stat_1N_4N_filter,'P-value', P_val_1N_4N_filter,'Sample size N', N_MJ_filter_1N_4N)
-    
+T_sign_1N_4N_filter, P_sign_1N_4N_filter = scipy.stats.wilcoxon(MJ_filter_1N_to_4_comon, MJ_filter_1N_to_4_comon)      
 
 T_stat_2N_3N, P_val_2N_3N = scipy.stats.ttest_ind(MJ_Phase_2N_to_3_comon,MJ_Phase_3N_to_2_comon, axis=0, equal_var=True)
 degree_2N_3N = (N_MJ_Phase_2N_3N -1) *Level_of_confidence
@@ -334,14 +337,15 @@ if degree_2N_3N < abs(T_stat_2N_3N):
     print('2N and 3N Phase rejects the null', T_stat_2N_3N,'P-value', P_val_2N_3N,'Sample size N', N_MJ_Phase_2N_3N)
 else:
     print('2N and 3N Phase accepts the null', T_stat_2N_3N,'P-value', P_val_2N_3N,'Sample size N', N_MJ_Phase_2N_3N)
-    
+T_sign_2N_3N, P_sign_2N_3N = scipy.stats.wilcoxon(MJ_Phase_2N_to_3_comon, MJ_Phase_3N_to_2_comon)
+
 T_stat_2N_3N_filter, P_val_2N_3N_filter = scipy.stats.ttest_ind(MJ_filter_2N_to_3_comon,MJ_filter_3N_to_2_comon, axis=0, equal_var=True)
 degree_2N_3N_filter = (N_MJ_filter_2N_3N -1) *Level_of_confidence
 if degree_2N_3N_filter < abs(T_stat_2N_3N_filter):
     print('2N and 3N Filter rejects the null', T_stat_2N_3N_filter,'P-value', P_val_2N_3N_filter,'Sample size N', N_MJ_filter_2N_3N)
 else:
     print('2N and 3N Filter accepts the null', T_stat_2N_3N_filter,'P-value', P_val_2N_3N_filter,'Sample size N', N_MJ_filter_2N_3N)
-    
+T_sign_2N_3N_filter, P_sign_2N_3N_filter = scipy.stats.wilcoxon(MJ_filter_2N_to_3_comon, MJ_filter_3N_to_2_comon)    
     
 T_stat_3N_4N, P_val_3N_4N = scipy.stats.ttest_ind(MJ_Phase_3N_to_4_comon,MJ_Phase_4N_to_3_comon, axis=0, equal_var=True)
 degree_3N_4N = (N_MJ_Phase_3N_4N -1) *Level_of_confidence
@@ -349,6 +353,7 @@ if degree_3N_4N < abs(T_stat_3N_4N):
     print('3N and 4N Phase rejects the null', T_stat_3N_4N,'P-value', P_val_3N_4N,'Sample size N', N_MJ_Phase_3N_4N)
 else:
     print('3N and 4N Phase accepts the null', T_stat_3N_4N,'P-value', P_val_3N_4N,'Sample size N', N_MJ_Phase_3N_4N)
+T_sign_3N_4N, P_sign_3N_4N = scipy.stats.wilcoxon(MJ_Phase_3N_to_4_comon, MJ_Phase_4N_to_3_comon)  
     
 T_stat_3N_4N_filter, P_val_3N_4N_filter = scipy.stats.ttest_ind(MJ_filter_3N_to_4_comon,MJ_filter_4N_to_3_comon, axis=0, equal_var=True)
 degree_3N_4N_filter = (N_MJ_filter_3N_4N -1) *Level_of_confidence
@@ -356,13 +361,37 @@ if degree_3N_4N_filter < abs(T_stat_3N_4N_filter):
     print('3N and 4N Filter rejects the null', T_stat_3N_4N_filter,'P-value', P_val_3N_4N_filter,'Sample size N', N_MJ_filter_3N_4N)
 else:
     print('3N and 4N Filter accepts the null', T_stat_3N_4N_filter,'P-value', P_val_3N_4N_filter,'Sample size N', N_MJ_filter_3N_4N)
-    
-#sns.distplot(Mj_1N_Phase, hist=True, rug=True)
-#sns.distplot(Mj_2N_Phase ,hist=True, rug=True)
-#sns.distplot(Mj_3N_Phase , hist=True, rug=True)
-#sns.distplot(Mj_4N_Phase , hist=True, rug=True)
+T_sign_3N_4N_filter, P_sign_3N_4N_filter = scipy.stats.wilcoxon(MJ_filter_3N_to_4_comon, MJ_filter_4N_to_3_comon)    
 
-#plt.show()
+whole_t_stat = [T_stat_1N_2N, T_stat_1N_3N, T_stat_1N_4N, T_stat_2N_3N, T_stat_3N_4N] 
+whole_p_test = [P_val_1N_2N,P_val_1N_3N,P_val_1N_4N,P_val_2N_3N,P_val_3N_4N]
+Whole_sample = [N_MJ_Phase_1N_2N, N_MJ_Phase_1N_3N, N_MJ_Phase_1N_4N, N_MJ_Phase_2N_3N, N_MJ_Phase_3N_4N]
+Whole_degree = [degree_1N_2N, degree_1N_3N, degree_1N_4N, degree_2N_3N, degree_3N_4N]
+Whole_sighn_t_stat = [T_sign_1N_2N,T_sign_1N_3N,T_sign_1N_4N,T_sign_2N_3N,T_sign_3N_4N]
+Whole_sighn_p_test = [P_sign_1N_2N,P_sign_1N_3N,P_sign_1N_4N,P_sign_2N_3N,P_sign_3N_4N]
+
+
+
+Non_filtered_no_hood = {'Phase':['1n-2N','1n-3n','1n-4n','2n-3n', '3n-4n'],'T-statistic':whole_t_stat, 'P Value':whole_p_test,
+                'T-statistic-Sign-Test':Whole_sighn_t_stat, 'P Vaue-Sign Test':Whole_sighn_p_test,
+                'Deggree of Confidence':Whole_degree, 'Sample Size':Whole_sample  }
+
+df_Non_filtered_no_hood = pd.DataFrame(Non_filtered_no_hood, columns=['Phase','T-statistic','P Value','T-statistic-Sign-Test',
+                                                              'P Vaue-Sign Test','Deggree of Confidence','Sample Size'])
+
+whole_t_stat_filter = [T_stat_1N_2N_filter, T_stat_1N_3N_filter, T_stat_1N_4N_filter, T_stat_2N_3N_filter, T_stat_3N_4N_filter] 
+whole_p_test_filter = [P_val_1N_2N_filter,P_val_1N_3N_filter,P_val_1N_4N_filter,P_val_2N_3N_filter,P_val_3N_4N_filter]
+Whole_sample_filter = [N_MJ_Phase_1N_2N, N_MJ_Phase_1N_3N, N_MJ_Phase_1N_4N, N_MJ_Phase_2N_3N, N_MJ_Phase_3N_4N]
+Whole_degree_filter = [degree_1N_2N, degree_1N_3N, degree_1N_4N, degree_2N_3N, degree_3N_4N]
+Whole_sighn_t_stat_filter = [T_sign_1N_2N_filter,T_sign_1N_3N_filter,T_sign_1N_4N_filter,T_sign_2N_3N_filter,T_sign_3N_4N_filter]
+Whole_sighn_p_test_filter = [P_sign_1N_2N_filter ,P_sign_1N_3N_filter,P_sign_1N_4N_filter,P_sign_2N_3N_filter,P_sign_3N_4N_filter]
+
+filtered_No_hood = {'Phase Filtered ':['1n-2N - Filter','1n-3n - Filter','1n-4n - Filter','2n-3n - Filter', '3n-4n - Filter'],'T-statistic':whole_t_stat_filter, 'P Value':whole_p_test_filter,
+                'T-statistic-Sign-Test':Whole_sighn_t_stat_filter, 'P Vaue-Sign Test':Whole_sighn_p_test_filter,
+                'Deggree of Confidence':Whole_degree_filter, 'Sample Size':Whole_sample_filter  }
+
+df_filtered_No_hood = pd.DataFrame(filtered_No_hood, columns=['Phase Filtered ' ,'T-statistic','P Value','T-statistic-Sign-Test',
+                                                              'P Vaue-Sign Test','Deggree of Confidence','Sample Size'])
 
 Kj_per_sae_no_hood = {'median':[np.median(Mj_1N_Phase),np.median(Mj_2N_Phase),np.median(Mj_3N_Phase),np.median(Mj_4N_Phase)],
                         'Phase':['1n','2n','3n','4n']}
@@ -381,16 +410,20 @@ print(pd.DataFrame(Kj_per_sae_mean_no_hood))
 
 ###hood
 print('Hood section')
-Kj_per_sae_Hood = {'median':[np.median(Mj_1N_Phase),np.median(Mj_2N_Phase),np.median(Mj_3N_Phase),np.median(Mj_4N_Phase)],
-                        'Phase':['1n','2n','3n','4n']}
+Kj_per_sae_Hood = {'median':[np.median(Mj_1H_Phase),np.median(Mj_2H_Phase),np.median(Mj_3H_Phase)],
+                        'Phase':['1H','2H','3H']}
 
 print(pd.DataFrame(Kj_per_sae_Hood))
 
-Kj_per_sae_filter_Hood = {'median filter':[np.median(Mj_filter_1N_Phase),np.median(Mj_filter_2N_Phase),np.median(Mj_filter_3N_Phase),np.median(Mj_filter_4N_Phase)],
-                        'Phase':['1n','2n','3n','4n']}
+Kj_per_sae_filter_Hood = {'median filter':[np.median(Mj_filter_1H_Phase),np.median(Mj_filter_2H_Phase),np.median(Mj_filter_3H_Phase)],
+                        'Phase':['1H','2H','3H']}
 
 print(pd.DataFrame(Kj_per_sae_filter_Hood))
 
-Kj_per_sae_mean_Hood = {'mean':[np.mean(Mj_1N_Phase),np.mean(Mj_2N_Phase),np.mean(Mj_3N_Phase),np.mean(Mj_4N_Phase)],
-                        'Phase':['1n','2n','3n','4n']}
+Kj_per_sae_mean_Hood = {'mean':[np.mean(Mj_1H_Phase),np.mean(Mj_2H_Phase),np.mean(Mj_3H_Phase)],
+                        'Phase':['1H','2H','3H']}
 print(pd.DataFrame(Kj_per_sae_mean_Hood))
+
+pATH = "C:/Users/rossgra/Box/OSU, CSC, CQC Project files/P_TEST_NO_HOOD.csv"
+df_Non_filtered_no_hood.to_csv(pATH, index=False,mode='a')
+df_filtered_No_hood.to_csv(pATH, index=False,mode='a')

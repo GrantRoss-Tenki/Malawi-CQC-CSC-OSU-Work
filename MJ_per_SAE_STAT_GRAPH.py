3f02843c38e6e64283e3ff614281d3e9b9c7272f
GrantRoss-Tenki <97212730+GrantRoss-Tenki@users.noreply.github.com>
2022-03-28 17:19:28 -0700

    Update MJ_per_SAE_STAT_GRAPH.py
    Added 2N to 4N pulling to the personal computer

--- MJ_per_SAE_STAT_GRAPH.py
+++ MJ_per_SAE_STAT_GRAPH.py
@@ -22,11 +22,11 @@ metric = input('SAE or Non   - ')
 #Hood_MJ_Path = "C:/Users/gvros/Box/OSU, CSC, CQC Project files/MJ per SAE - Hood.csv"
 #### for FUEL_REMOVED _perd
 if metric== 'SAE':
-    No_hood_MJ_path = "C:/Users/gvros/Box/OSU, CSC, CQC Project files/24 Hour Remove SAE - No_Hood.csv" #rossgra or gvros
-    Hood_MJ_Path = "C:/Users/gvros/Box/OSU, CSC, CQC Project files/24 Hour Remove SAE - Hood.csv"
+    No_hood_MJ_path = "C:/Users/rossgra/Box/OSU, CSC, CQC Project files/24 Hour Remove SAE - No_Hood.csv" #rossgra or gvros
+    Hood_MJ_Path = "C:/Users/rossgra/Box/OSU, CSC, CQC Project files/24 Hour Remove SAE - Hood.csv"
 else:
-    No_hood_MJ_path = "C:/Users/gvros/Box/OSU, CSC, CQC Project files/24 Hour Remove - No_Hood.csv" #rossgra or gvros
-    Hood_MJ_Path = "C:/Users/gvros/Box/OSU, CSC, CQC Project files/24 Hour Remove - Hood.csv"
+    No_hood_MJ_path = "C:/Users/rossgra/Box/OSU, CSC, CQC Project files/24 Hour Remove - No_Hood.csv" #rossgra or gvros
+    Hood_MJ_Path = "C:/Users/rossgra/Box/OSU, CSC, CQC Project files/24 Hour Remove - Hood.csv"
 ######For Fuel removed per 24 hours per SAE
 
 
@@ -398,7 +398,39 @@ for row_2H, hh_2H in enumerate(HH_2H):
             
 N_MJ_filter_2H_3H = count_n - 1
 
+#2N to 4N
 
+MJ_Phase_2N_to_4_comon = []
+MJ_Phase_4N_to_2_comon = []
+Day_count_MJ_Phase_2N_4N = []
+count_n = 0
+for row_2N, hh_2N in enumerate(HH_2N):
+    if hh_2N == str(-1):
+        break
+    for row_4N, hh_4N in enumerate(HH_4N):
+        if hh_2N == hh_4N:
+            MJ_Phase_2N_to_4_comon.append(Mj_2N_Phase[row_2N])
+            MJ_Phase_4N_to_2_comon.append(Mj_4N_Phase[row_4N])
+            Day_count_MJ_Phase_2N_4N.append(Phase_2N_day_count[row_2N] +Phase_4N_day_count[row_4N] )
+            count_n = count_n + 1
+
+N_MJ_Phase_2N_4N = count_n -1
+#for filter 
+MJ_filter_2N_to_4_comon = []
+MJ_filter_4N_to_2_comon = []
+Day_count_MJ_filter_2N_4N = []
+count_n = 0
+for row_2N, hh_2N in enumerate(HH_2N):
+    if hh_2N == str(-1):
+        break
+    for row_4N, hh_4N in enumerate(HH_4N):
+        if hh_2N == hh_4N:
+            MJ_filter_2N_to_4_comon.append(Mj_filter_2N_Phase[row_2N])
+            MJ_filter_4N_to_2_comon.append(Mj_filter_4N_Phase[row_4N])
+            Day_count_MJ_filter_2N_4N.append(Filter_2N_day_count[row_2N] +Filter_4N_day_count[row_4N] )
+            count_n = count_n + 1
+            
+N_MJ_filter_2N_4N = count_n - 1
 
 #3N to 4N
 
@@ -562,7 +594,25 @@ else:
     print('2H and 3H Filter accepts the null', T_stat_2H_3H_filter,'P-value', P_val_2H_3H_filter,'Sample size N', N_MJ_filter_2H_3H)
 T_sign_2H_3H_filter, P_sign_2H_3H_filter = scipy.stats.wilcoxon(MJ_filter_2H_to_3_comon, MJ_filter_3H_to_2_comon)
 
+#2N to 4N
 
+T_stat_2N_4N, P_val_2N_4N = scipy.stats.ttest_ind(MJ_Phase_2N_to_4_comon,MJ_Phase_4N_to_2_comon, axis=0, equal_var=True)
+degree_2N_4N = (N_MJ_Phase_2N_4N -1) *Level_of_confidence
+if degree_2N_4N < abs(T_stat_2N_4N):
+    print('2N and 4N Phase rejects the null', T_stat_2N_4N,'P-value', P_val_2N_4N,'Sample size N', N_MJ_Phase_2N_4N)
+else:
+    print('2N and 4N Phase accepts the null', T_stat_2N_4N,'P-value', P_val_2N_4N,'Sample size N', N_MJ_Phase_2N_4N)
+T_sign_2N_4N, P_sign_2N_4N = scipy.stats.wilcoxon(MJ_Phase_2N_to_4_comon, MJ_Phase_4N_to_2_comon)  
+    
+T_stat_2N_4N_filter, P_val_2N_4N_filter = scipy.stats.ttest_ind(MJ_filter_2N_to_4_comon,MJ_filter_4N_to_2_comon, axis=0, equal_var=True)
+degree_2N_4N_filter = (N_MJ_filter_2N_4N -1) *Level_of_confidence
+if degree_2N_4N_filter < abs(T_stat_2N_4N_filter):
+    print('2N and 4N Filter rejects the null', T_stat_2N_4N_filter,'P-value', P_val_2N_4N_filter,'Sample size N', N_MJ_filter_2N_4N)
+else:
+    print('2N and 4N Filter accepts the null', T_stat_2N_4N_filter,'P-value', P_val_2N_4N_filter,'Sample size N', N_MJ_filter_2N_4N)
+T_sign_2N_4N_filter, P_sign_2N_4N_filter = scipy.stats.wilcoxon(MJ_filter_2N_to_4_comon, MJ_filter_4N_to_2_comon)    
+
+#3N to 4N
 
 T_stat_3N_4N, P_val_3N_4N = scipy.stats.ttest_ind(MJ_Phase_3N_to_4_comon,MJ_Phase_4N_to_3_comon, axis=0, equal_var=True)
 degree_3N_4N = (N_MJ_Phase_3N_4N -1) *Level_of_confidence
@@ -580,19 +630,19 @@ else:
     print('3N and 4N Filter accepts the null', T_stat_3N_4N_filter,'P-value', P_val_3N_4N_filter,'Sample size N', N_MJ_filter_3N_4N)
 T_sign_3N_4N_filter, P_sign_3N_4N_filter = scipy.stats.wilcoxon(MJ_filter_3N_to_4_comon, MJ_filter_4N_to_3_comon)    
 
-whole_t_stat = [T_stat_1N_2N, T_stat_1N_3N, T_stat_1N_4N, T_stat_2N_3N, T_stat_3N_4N] 
-whole_p_test = [P_val_1N_2N,P_val_1N_3N,P_val_1N_4N,P_val_2N_3N,P_val_3N_4N]
-Whole_sample = [N_MJ_Phase_1N_2N, N_MJ_Phase_1N_3N, N_MJ_Phase_1N_4N, N_MJ_Phase_2N_3N, N_MJ_Phase_3N_4N]
-Whole_degree = [degree_1N_2N, degree_1N_3N, degree_1N_4N, degree_2N_3N, degree_3N_4N]
-Whole_sighn_t_stat = [T_sign_1N_2N,T_sign_1N_3N,T_sign_1N_4N,T_sign_2N_3N,T_sign_3N_4N]
-Whole_sighn_p_test = [P_sign_1N_2N,P_sign_1N_3N,P_sign_1N_4N,P_sign_2N_3N,P_sign_3N_4N]
+whole_t_stat = [T_stat_1N_2N, T_stat_1N_3N, T_stat_1N_4N, T_stat_2N_3N, T_stat_3N_4N,T_stat_2N_4N] 
+whole_p_test = [P_val_1N_2N,P_val_1N_3N,P_val_1N_4N,P_val_2N_3N,P_val_3N_4N,P_val_2N_4N]
+Whole_sample = [N_MJ_Phase_1N_2N, N_MJ_Phase_1N_3N, N_MJ_Phase_1N_4N, N_MJ_Phase_2N_3N, N_MJ_Phase_3N_4N,N_MJ_Phase_2N_4N]
+Whole_degree = [degree_1N_2N, degree_1N_3N, degree_1N_4N, degree_2N_3N, degree_3N_4N, degree_2N_4N]
+Whole_sighn_t_stat = [T_sign_1N_2N,T_sign_1N_3N,T_sign_1N_4N,T_sign_2N_3N,T_sign_3N_4N,T_sign_2N_4N]
+Whole_sighn_p_test = [P_sign_1N_2N,P_sign_1N_3N,P_sign_1N_4N,P_sign_2N_3N,P_sign_3N_4N, P_sign_3N_4N]
 
-STD_1 = [np.std(MJ_Phase_1N_to_2_comon), np.std(MJ_Phase_1N_to_3_comon),np.std(MJ_Phase_1N_to_4_comon),np.std(MJ_Phase_2N_to_3_comon),np.std(MJ_Phase_3N_to_4_comon)]
-Median_1 = [stat.median(MJ_Phase_1N_to_2_comon), stat.median(MJ_Phase_1N_to_3_comon),stat.median(MJ_Phase_1N_to_4_comon),stat.median(MJ_Phase_2N_to_3_comon),stat.median(MJ_Phase_3N_to_4_comon)]
-Mean_1 = [np.average(MJ_Phase_1N_to_2_comon),np.average(MJ_Phase_1N_to_3_comon),np.average(MJ_Phase_1N_to_4_comon),np.average(MJ_Phase_2N_to_3_comon),np.average(MJ_Phase_3N_to_4_comon)]
-STD_2 = [np.std(MJ_Phase_2N_to_1_comon), np.std(MJ_Phase_3N_to_1_comon),np.std(MJ_Phase_4N_to_1_comon),np.std(MJ_Phase_3N_to_2_comon),np.std(MJ_Phase_4N_to_3_comon)]
-Median_2 = [stat.median(MJ_Phase_2N_to_1_comon), stat.median(MJ_Phase_3N_to_1_comon),stat.median(MJ_Phase_4N_to_1_comon),stat.median(MJ_Phase_3N_to_2_comon),stat.median(MJ_Phase_4N_to_3_comon)]
-Mean_2 = [np.average(MJ_Phase_2N_to_1_comon),np.average(MJ_Phase_3N_to_1_comon),np.average(MJ_Phase_4N_to_1_comon),np.average(MJ_Phase_3N_to_2_comon),np.average(MJ_Phase_4N_to_3_comon)]
+STD_1 = [np.std(MJ_Phase_1N_to_2_comon), np.std(MJ_Phase_1N_to_3_comon),np.std(MJ_Phase_1N_to_4_comon),np.std(MJ_Phase_2N_to_3_comon),np.std(MJ_Phase_3N_to_4_comon),np.std(MJ_Phase_2N_to_4_comon)]
+Median_1 = [stat.median(MJ_Phase_1N_to_2_comon), stat.median(MJ_Phase_1N_to_3_comon),stat.median(MJ_Phase_1N_to_4_comon),stat.median(MJ_Phase_2N_to_3_comon),stat.median(MJ_Phase_3N_to_4_comon), stat.median(MJ_Phase_2N_to_4_comon)]
+Mean_1 = [np.average(MJ_Phase_1N_to_2_comon),np.average(MJ_Phase_1N_to_3_comon),np.average(MJ_Phase_1N_to_4_comon),np.average(MJ_Phase_2N_to_3_comon),np.average(MJ_Phase_3N_to_4_comon),np.average(MJ_Phase_2N_to_4_comon)]
+STD_2 = [np.std(MJ_Phase_2N_to_1_comon), np.std(MJ_Phase_3N_to_1_comon),np.std(MJ_Phase_4N_to_1_comon),np.std(MJ_Phase_3N_to_2_comon),np.std(MJ_Phase_4N_to_3_comon),np.std(MJ_Phase_4N_to_2_comon)]
+Median_2 = [stat.median(MJ_Phase_2N_to_1_comon), stat.median(MJ_Phase_3N_to_1_comon),stat.median(MJ_Phase_4N_to_1_comon),stat.median(MJ_Phase_3N_to_2_comon),stat.median(MJ_Phase_4N_to_3_comon),stat.median(MJ_Phase_4N_to_2_comon)]
+Mean_2 = [np.average(MJ_Phase_2N_to_1_comon),np.average(MJ_Phase_3N_to_1_comon),np.average(MJ_Phase_4N_to_1_comon),np.average(MJ_Phase_3N_to_2_comon),np.average(MJ_Phase_4N_to_3_comon),np.average(MJ_Phase_4N_to_2_comon)]
 
 No_hood_percent_days_Filtered = [sum(Filter_1N_day_count)/sum(Phase_1N_day_count),sum(Filter_2N_day_count)/sum(Phase_2N_day_count),sum(Filter_3N_day_count)/sum(Phase_3N_day_count),sum(Filter_4N_day_count)/sum(Phase_4N_day_count) ]
 hood_percent_days_Filtered = [sum(Filter_1H_day_count)/sum(Phase_1H_day_count),sum(Filter_2H_day_count)/sum(Phase_2H_day_count),sum(Filter_3H_day_count)/sum(Phase_3H_day_count)]
@@ -618,7 +668,7 @@ Mean_2_H = [np.average(MJ_Phase_2H_to_1_comon),np.average(MJ_Phase_3H_to_1_comon
 
 
 
-Non_filtered_no_hood = {'Phase':['1n-2N','1n-3n','1n-4n','2n-3n', '3n-4n'],'T-statistic':whole_t_stat, 'P Value':whole_p_test,
+Non_filtered_no_hood = {'Phase':['1n-2N','1n-3n','1n-4n','2n-3n', '3n-4n','2n-4n'],'T-statistic':whole_t_stat, 'P Value':whole_p_test,
                 'T-statistic-Sign-Test':Whole_sighn_t_stat, 'P Vaue-Sign Test':Whole_sighn_p_test,
                 'Deggree of Confidence':Whole_degree, 'Sample Size':Whole_sample,'Std _1':STD_1,'median _1':Median_1,'mean _1':Mean_1,'Std _2':STD_2,'median _2':Median_2,'mean _2':Mean_2   }
 
@@ -634,12 +684,12 @@ df_Non_filtered_hood = pd.DataFrame(Non_filtered_hood, columns=['Phase _Hood','T
                                                               'P Vaue-Sign Test','Deggree of Confidence','Sample Size', 'Std _1','median _1','mean _1','Std _2','median _2','mean _2'])
 
 
-whole_t_stat_filter = [T_stat_1N_2N_filter, T_stat_1N_3N_filter, T_stat_1N_4N_filter, T_stat_2N_3N_filter, T_stat_3N_4N_filter] 
-whole_p_test_filter = [P_val_1N_2N_filter,P_val_1N_3N_filter,P_val_1N_4N_filter,P_val_2N_3N_filter,P_val_3N_4N_filter]
-Whole_sample_filter = [N_MJ_Phase_1N_2N, N_MJ_Phase_1N_3N, N_MJ_Phase_1N_4N, N_MJ_Phase_2N_3N, N_MJ_Phase_3N_4N]
-Whole_degree_filter = [degree_1N_2N, degree_1N_3N, degree_1N_4N, degree_2N_3N, degree_3N_4N]
-Whole_sighn_t_stat_filter = [T_sign_1N_2N_filter,T_sign_1N_3N_filter,T_sign_1N_4N_filter,T_sign_2N_3N_filter,T_sign_3N_4N_filter]
-Whole_sighn_p_test_filter = [P_sign_1N_2N_filter ,P_sign_1N_3N_filter,P_sign_1N_4N_filter,P_sign_2N_3N_filter,P_sign_3N_4N_filter]
+whole_t_stat_filter = [T_stat_1N_2N_filter, T_stat_1N_3N_filter, T_stat_1N_4N_filter, T_stat_2N_3N_filter, T_stat_3N_4N_filter, T_stat_2N_4N_filter] 
+whole_p_test_filter = [P_val_1N_2N_filter,P_val_1N_3N_filter,P_val_1N_4N_filter,P_val_2N_3N_filter,P_val_3N_4N_filter, P_val_2N_4N_filter]
+Whole_sample_filter = [N_MJ_Phase_1N_2N, N_MJ_Phase_1N_3N, N_MJ_Phase_1N_4N, N_MJ_Phase_2N_3N, N_MJ_Phase_3N_4N, N_MJ_Phase_2N_4N]
+Whole_degree_filter = [degree_1N_2N, degree_1N_3N, degree_1N_4N, degree_2N_3N, degree_3N_4N, degree_2N_4N]
+Whole_sighn_t_stat_filter = [T_sign_1N_2N_filter,T_sign_1N_3N_filter,T_sign_1N_4N_filter,T_sign_2N_3N_filter,T_sign_3N_4N_filter,T_sign_2N_4N_filter]
+Whole_sighn_p_test_filter = [P_sign_1N_2N_filter ,P_sign_1N_3N_filter,P_sign_1N_4N_filter,P_sign_2N_3N_filter,P_sign_3N_4N_filter,P_sign_2N_4N_filter]
 
 whole_t_stat_filter_H = [T_stat_1H_2H_filter, T_stat_1H_3H_filter, T_stat_2H_3H_filter] 
 whole_p_test_filter_H = [P_val_1H_2H_filter,P_val_1H_3H_filter,P_val_2H_3H_filter]
@@ -648,7 +698,7 @@ Whole_degree_filter_H = [degree_1H_2H, degree_1H_3H, degree_2H_3H]
 Whole_sighn_t_stat_filter_H = [T_sign_1H_2H_filter,T_sign_1H_3H_filter,T_sign_2H_3H_filter]
 Whole_sighn_p_test_filter_H = [P_sign_1H_2H_filter ,P_sign_1H_3H_filter,P_sign_2H_3H_filter]
 
-filtered_No_hood = {'Phase Filtered ':['1n-2N - Filter','1n-3n - Filter','1n-4n - Filter','2n-3n - Filter', '3n-4n - Filter'],'T-statistic':whole_t_stat_filter, 'P Value':whole_p_test_filter,
+filtered_No_hood = {'Phase Filtered ':['1n-2N - Filter','1n-3n - Filter','1n-4n - Filter','2n-3n - Filter', '3n-4n - Filter','2n-4n - Filter'],'T-statistic':whole_t_stat_filter, 'P Value':whole_p_test_filter,
                 'T-statistic-Sign-Test':Whole_sighn_t_stat_filter, 'P Vaue-Sign Test':Whole_sighn_p_test_filter,
                 'Deggree of Confidence':Whole_degree_filter, 'Sample Size':Whole_sample_filter }
 
@@ -706,23 +756,23 @@ Kj_per_sae_mean_Hood = {'mean':[np.mean(Mj_1H_Phase),np.mean(Mj_2H_Phase),np.mea
                         'Phase':['1H','2H','3H']}
 df_Kj_per_sae_mean_Hood = pd.DataFrame(Kj_per_sae_mean_Hood)
 
-#pATH = "C:/Users/gvros/Box/OSU, CSC, CQC Project files/P_TEST_NO_HOOD_24hour_SAE.csv"
-#df_Non_filtered_no_hood.to_csv(pATH, index=False,mode='a')
-#df_filtered_No_hood.to_csv(pATH, index=False,mode='a')
+pATH = "C:/Users/rossgra /Box/OSU, CSC, CQC Project files/P_TEST_NO_HOOD_24hour_SAE_with_2n_4N.csv"
+df_Non_filtered_no_hood.to_csv(pATH, index=False,mode='a')
+df_filtered_No_hood.to_csv(pATH, index=False,mode='a')
 
-#df_Non_filtered_hood.to_csv(pATH, index=False,mode='a')
-#df_filtered_hood.to_csv(pATH, index=False,mode='a')
-#df_percent_hood.to_csv(pATH, index=False,mode='a')
-#df_percent_No_hood.to_csv(pATH, index=False,mode='a')
+df_Non_filtered_hood.to_csv(pATH, index=False,mode='a')
+df_filtered_hood.to_csv(pATH, index=False,mode='a')
+df_percent_hood.to_csv(pATH, index=False,mode='a')
+df_percent_No_hood.to_csv(pATH, index=False,mode='a')
 
-#df_Kj_per_sae_no_hood.to_csv(pATH, index=False,mode='a')
-#df_Kj_per_sae_filter_no_hood.to_csv(pATH, index=False,mode='a')
-#df_Kj_per_sae_mean_filter_no_hood.to_csv(pATH, index=False,mode='a')
-#df_Kj_per_sae_mean_no_hood.to_csv(pATH, index=False,mode='a')
-#df_Kj_per_sae_Hood.to_csv(pATH, index=False,mode='a')
-#df_Kj_per_sae_filter_Hood.to_csv(pATH, index=False,mode='a')
-#df_Kj_per_sae_mean_Hood.to_csv(pATH, index=False,mode='a')
-#df_Kj_per_mean_filter_Hood.to_csv(pATH, index=False,mode='a')
+df_Kj_per_sae_no_hood.to_csv(pATH, index=False,mode='a')
+df_Kj_per_sae_filter_no_hood.to_csv(pATH, index=False,mode='a')
+df_Kj_per_sae_mean_filter_no_hood.to_csv(pATH, index=False,mode='a')
+df_Kj_per_sae_mean_no_hood.to_csv(pATH, index=False,mode='a')
+df_Kj_per_sae_Hood.to_csv(pATH, index=False,mode='a')
+df_Kj_per_sae_filter_Hood.to_csv(pATH, index=False,mode='a')
+df_Kj_per_sae_mean_Hood.to_csv(pATH, index=False,mode='a')
+df_Kj_per_mean_filter_Hood.to_csv(pATH, index=False,mode='a')
 
 MJ_Phase_1N_to_3_comon
 Mj_filter_3N_Phase

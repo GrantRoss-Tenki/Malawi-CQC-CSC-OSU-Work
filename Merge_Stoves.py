03d00c322750959e8d24425985406d4ce80b406c
GrantRoss-Tenki <97212730+GrantRoss-Tenki@users.noreply.github.com>
2022-05-01 19:19:43 -0700

    Update Merge_Stoves.py
    small changes

--- Merge_Stoves.py
+++ Merge_Stoves.py
@@ -334,9 +334,9 @@ for file in csv_R_m:
             K_H_MIN_tv, K_H_MAX_tv ,K_H_MIN_Count, K_H_MAX_Count  = Functions_malawi.Local_Max_min(Raw_Combined_Kitchen_Hapex[event_num], start)
             K_Hapex_Startup_max, K_Hapex_Next_Startup_min = Functions_malawi.StartUp_max_Next_min(Raw_Combined_Kitchen_Hapex[event_num], start)
             #print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~',K_H_MAX_Count, K_H_MIN_Count )
-            Steady_start_Time_value = Functions_malawi.SteadyState_Finder(Raw_Combined_Kitchen_Hapex[event_num], 7, K_H_MIN_Count, K_H_MAX_Count,K_Hapex_Startup_max,start  )
+            Steady_start_Time_value = Functions_malawi.SteadyState_Finder(Raw_Combined_Kitchen_Hapex[event_num], 7, K_H_MIN_Count, K_H_MAX_Count,start  )
 
-            #print('here is the time vlaue for the steady state', Steady_start_Time_value)
+            print('here is the time vlaue for the steady state', Steady_start_Time_value)
 
             Whole_Kitchen_hapex_Min_tv.append(K_H_MIN_tv + (start-10))
             Whole_Kitchen_hapex_Max_tv.append(K_H_MAX_tv + (start-10))

11a7c85f68c3250acced7495fa7c4002d21657dc
GrantRoss-Tenki <97212730+GrantRoss-Tenki@users.noreply.github.com>
2022-05-26 11:47:46 -0700

    Update Tesing_Hapex_Algorithm.py
    adding Heathers median of the gradient.... personally, I think this is a lazy way of cross sensor analysis

--- Tesing_Hapex_Algorithm.py
+++ Tesing_Hapex_Algorithm.py
@@ -11,8 +11,28 @@ import seaborn as sns
 from pathlib import Path, PureWindowsPath
 import Functions_malawi
 
+Phase  = "1N"
+Houshold = 1007
+start_spread = 10
+cooldown_Length = 30
+
+
 path = "C:/Users/gvros/Desktop/Practice_HAPEX_detection_Array.csv"
 csv_read = pd.read_csv(path)
+
+Path_CE_MErge = "C:/Users/gvros/Desktop/Oregon State Masters/Work/OSU, CSC, CQC Project files/"+Phase+"/Compiler_1_exact/Raw_D_metrics/"+Phase+"_HH_raw_Day_metrics_"+str(Houshold)+"_1_exact_3.55555.csv"
+Event_dataframe= pd.read_csv(Path_CE_MErge,skiprows = 2 )
+
+ff_usage = Event_dataframe.iloc[:,3]
+ff_temp = Event_dataframe.iloc[:,4]
+Kitchen_Hapex = Event_dataframe.iloc[:,8]
+
+Gradient_Hapex = (np.gradient(Kitchen_Hapex))
+
+ABS_Gradient_Hapex = abs(Gradient_Hapex)
+Median_Gradient_Hapex = np.median(ABS_Gradient_Hapex)
+print('=-=-=-=-=-=-=-=-=-=-==-=-=-',Median_Gradient_Hapex)
+
 csv = csv_read.iloc[:,0]
 start = 10
 K_H_MIN_tv, K_H_MAX_tv ,K_H_MIN_Count, K_H_MAX_Count  = Functions_malawi.Local_Max_min(csv, start)

f1f143bc3fda5aee3103583edcbcfe853ed62632
GrantRoss-Tenki <97212730+GrantRoss-Tenki@users.noreply.github.com>
2022-05-26 11:46:52 -0700

    Update CE_Sensitivity_USB_4N.py
    added some thresholds and values for sensitivity

--- CE_Sensitivity_USB_4N.py
+++ CE_Sensitivity_USB_4N.py
@@ -11,6 +11,7 @@ import seaborn as sns
 from pathlib import Path, PureWindowsPath
 import Functions_malawi
 from itertools import zip_longest
+import plotly.express as px
 import plotly.graph_objects as go
 
 
@@ -29,25 +30,35 @@ JFK_SUM_Time = []
 #    for line in csv.DictReader(data):
 #        print(line)
 
-# first round is for 1N and 1007
+# first round is for 1N and 10071007
 
-Path_CE_MErge = "E:/24_hour_pump/1N/Raw_pump_Time/Exact_1_1007_1N_.csv"
 
-Range = np.arange( 20, 90,1)
+Phase = "2N"
+Houshold = 1007
 
-EXACT_tester = pd.read_csv(Path_CE_MErge)
-TEMp = EXACT_tester.iloc[:,0]
-Usage = EXACT_tester.iloc[:,1]
+Path_CE_MErge = "C:/Users/gvros/Desktop/Oregon State Masters/Work/OSU, CSC, CQC Project files/"+Phase+"/Compiler_1_exact/Raw_D_metrics/"+Phase+"_HH_raw_Day_metrics_"+str(Houshold)+"_1_exact_3.55555.csv"
+Range = np.arange( 20, 60,1)
 
-cooking_threshold = 1
-length_decrease = 10
-start_threshold = 1
-end_threshold = -5
+EXACT_tester = pd.read_csv(Path_CE_MErge,skiprows = 2 )
+TEMp = EXACT_tester.iloc[:,4]
+Usage = EXACT_tester.iloc[:,3]
 
-#merge_CE_threshold = 40
+print('hafskfhkjeahfisekjfahsiufwa',EXACT_tester.iloc[2,0])
 
-min_CE_length = 8
-window_slope = 12
+if Phase  == ("2N") or Phase == "3N" or Phase == "3N" or Phase == "4N":
+    cooking_threshold = 5
+    length_decrease = 40
+    start_threshold = 1
+    end_threshold = -5
+    min_CE_length = 5
+    window_slope = 8
+else:
+    cooking_threshold = 1
+    length_decrease = 10
+    start_threshold = 1
+    end_threshold = -5
+    min_CE_length = 8
+    window_slope = 12
 
 SUM_cooking_sense = []
 

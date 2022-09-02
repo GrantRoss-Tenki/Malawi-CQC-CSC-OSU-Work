50b9087022df62897177146699e3578c3c58dca7
GrantRoss-Tenki <97212730+GrantRoss-Tenki@users.noreply.github.com>
2022-09-01 17:52:26 -0700

    Update CCT_Malawi_1.1_Synopsis.py
    working for HH TSF only, now need to include stir and flour in. need to add CQC and JFK

--- CCT_Malawi_1.1_Synopsis.py
+++ CCT_Malawi_1.1_Synopsis.py
@@ -38,9 +38,9 @@ CQC_JFK = pd.DataFrame(pd.read_csv(Path_Stove_3, names=COl_namnes, header =0))
 # Filtering out the non Standardized Water and Flour
 TSF_Filter = []
 TSF_Non_Filter = []
-Name_TSF = TSF['Household and Test']
-Wood_TSF = TSF['Wood Used'];Cooked_TSF = TSF['Cooked Food'];Charcoal_TSF = TSF['Hot Charcoal'];Flour_In_TSF = TSF['Start to Flour in']
-Stir_TSF = TSF['Start to Stir'];Boil_TSF = TSF['Start to Boil'];CE_Time_TSF = TSF['Cooking Time']
+Name_TSF = (TSF['Household and Test'])
+Wood_TSF = list(TSF['Wood Used']);Cooked_TSF = list(TSF['Cooked Food']);Charcoal_TSF = list(TSF['Hot Charcoal']);Flour_In_TSF = list(TSF['Start to Flour in'])
+Stir_TSF = list(TSF['Start to Stir']);Boil_TSF = list(TSF['Start to Boil']);CE_Time_TSF = list(TSF['Cooking Time'])
 
 for TSF_Row, TSF in enumerate(TSF['Flour Used']):
     if TSF == 1300:
@@ -50,9 +50,9 @@ for TSF_Row, TSF in enumerate(TSF['Flour Used']):
         
 CQC_Filter = []
 CQC_Non_Filter = []
-Name_CQC = CQC['Household and Test']
-Wood_CQC = CQC['Wood Used'];Cooked_CQC = CQC['Cooked Food'];Charcoal_CQC = CQC['Hot Charcoal'];Flour_In_CQC = CQC['Start to Flour in']
-Stir_CQC = CQC['Start to Stir'];Boil_CQC = CQC['Start to Boil'];CE_Time_CQC = CQC['Cooking Time']
+Name_CQC = (CQC['Household and Test'])
+Wood_CQC = list(CQC['Wood Used']);Cooked_CQC = list(CQC['Cooked Food']);Charcoal_CQC = list(CQC['Hot Charcoal']);Flour_In_CQC = list(CQC['Start to Flour in'])
+Stir_CQC = list(CQC['Start to Stir']);Boil_CQC = list(CQC['Start to Boil']);CE_Time_CQC = list(CQC['Cooking Time'])
 
 for CQC_Row, CQC in enumerate(CQC['Flour Used']):
     if CQC == 1300:
@@ -62,10 +62,10 @@ for CQC_Row, CQC in enumerate(CQC['Flour Used']):
         
 CQC_JFK_Filter = []
 CQC_JFK_Non_Filter = []
-Name_CQC_JFK = CQC_JFK['Household and Test'];
-Wood_CQC_JFK = CQC_JFK['Wood Used'];Cooked_CQC_JFK = CQC_JFK['Cooked Food'];Charcoal_CQC_JFK = CQC_JFK['Hot Charcoal']
-Flour_In_CQC_JFK = CQC_JFK['Start to Flour in'];Stir_CQC_JFK = CQC_JFK['Start to Stir'];Boil_CQC_JFK = CQC_JFK['Start to Boil']
-CE_Time_CQC_JFK = CQC_JFK['Cooking Time']
+Name_CQC_JFK = (CQC_JFK['Household and Test']);
+Wood_CQC_JFK = list(CQC_JFK['Wood Used']);Cooked_CQC_JFK = list(CQC_JFK['Cooked Food']);Charcoal_CQC_JFK = list(CQC_JFK['Hot Charcoal'])
+Flour_In_CQC_JFK = list(CQC_JFK['Start to Flour in']);Stir_CQC_JFK = list(CQC_JFK['Start to Stir']);Boil_CQC_JFK = list(CQC_JFK['Start to Boil'])
+CE_Time_CQC_JFK = list(CQC_JFK['Cooking Time'])
 
 for CQC_JFK_Row, CQC_JFK in enumerate(CQC_JFK['Flour Used']):
     if CQC_JFK == 1300:
@@ -77,46 +77,79 @@ for CQC_JFK_Row, CQC_JFK in enumerate(CQC_JFK['Flour Used']):
 
 # Three stone Fire Baseline Matrix Partition
 
-for TSF_val in TSF_Non_Filter:
-    #print('is the first loop for tstf working?  ', TSF_val, TSF_Filter)
+def delete_multiple_element(list_object, indices):
+    indices = sorted(indices, reverse=True)
+    for idx in indices:
+        if idx < len(list_object):
+            list_object.pop(idx)
+            
+
+
+delete_multiple_element(Name_TSF, TSF_Non_Filter)
+delete_multiple_element(Wood_TSF, TSF_Non_Filter)
+delete_multiple_element(Cooked_TSF, TSF_Non_Filter)
+delete_multiple_element(Charcoal_TSF, TSF_Non_Filter)
+delete_multiple_element(Flour_In_TSF, TSF_Non_Filter)
+delete_multiple_element(Stir_TSF, TSF_Non_Filter)
+delete_multiple_element(Boil_TSF, TSF_Non_Filter)
+delete_multiple_element(CE_Time_TSF, TSF_Non_Filter)
+
+#for TSF_val in TSF_Non_Filter:
+#    print('is the first loop for tstf working?  ', TSF_val, TSF_Filter)
     
-    Name_TSF.pop(TSF_val)
-    Wood_TSF.pop(TSF_val)
-    Cooked_TSF.pop(TSF_val)
-    Charcoal_TSF.pop(TSF_val)
-    Flour_In_TSF.pop(TSF_val)
-    Stir_TSF.pop(TSF_val)
-    Boil_TSF.pop(TSF_val)
-    CE_Time_TSF.pop(TSF_val)
+#    Name_TSF.pop(TSF_val)
+#    Wood_TSF.pop(TSF_val)
+#    Cooked_TSF.pop(TSF_val)
+#    Charcoal_TSF.pop(TSF_val)
+#    Flour_In_TSF.pop(TSF_val)
+#    Stir_TSF.pop(TSF_val)
+#    Boil_TSF.pop(TSF_val)
+#    CE_Time_TSF.pop(TSF_val)
 
 # CQC Stove Matrix Partition
+delete_multiple_element(Name_CQC, CQC_Non_Filter)
+delete_multiple_element(Wood_CQC, CQC_Non_Filter)
+delete_multiple_element(Cooked_CQC, CQC_Non_Filter)
+delete_multiple_element(Charcoal_CQC, CQC_Non_Filter)
+delete_multiple_element(Flour_In_CQC, CQC_Non_Filter)
+delete_multiple_element(Stir_CQC, CQC_Non_Filter)
+delete_multiple_element(Boil_CQC, CQC_Non_Filter)
+delete_multiple_element(CE_Time_CQC, CQC_Non_Filter)
 
-
-for CQC_val in CQC_Non_Filter:
-    Name_CQC.pop(CQC_val)
-    Wood_CQC.pop(CQC_val)
-    Cooked_CQC.pop(CQC_val)
-    Charcoal_CQC.pop(CQC_val)
-    Flour_In_CQC.pop(CQC_val)
-    Stir_CQC.pop(CQC_val)
-    Boil_CQC.pop(CQC_val)
-    CE_Time_CQC.pop(CQC_val)
-    
+#for CQC_val in CQC_Non_Filter:
+#    Name_CQC.pop(CQC_val)
+#    Wood_CQC.pop(CQC_val)
+#    Cooked_CQC.pop(CQC_val)
+#    Charcoal_CQC.pop(CQC_val)
+#    Flour_In_CQC.pop(CQC_val)
+#    Stir_CQC.pop(CQC_val)
+#    Boil_CQC.pop(CQC_val)
+#    CE_Time_CQC.pop(CQC_val)
+#    
 # CQC with Jet Flame Matrix Partition
+delete_multiple_element(Name_CQC_JFK, CQC_JFK_Non_Filter)
+delete_multiple_element(Wood_CQC_JFK, CQC_JFK_Non_Filter)
+delete_multiple_element(Cooked_CQC_JFK, CQC_JFK_Non_Filter)
+delete_multiple_element(Charcoal_CQC_JFK, CQC_JFK_Non_Filter)
+delete_multiple_element(Flour_In_CQC_JFK, CQC_JFK_Non_Filter)
+delete_multiple_element(Stir_CQC_JFK, CQC_JFK_Non_Filter)
+delete_multiple_element(Boil_CQC_JFK, CQC_JFK_Non_Filter)
+delete_multiple_element(CE_Time_CQC_JFK, CQC_JFK_Non_Filter)
 
 
-for JFK_val in CQC_JFK_Non_Filter:
-    Name_CQC_JFK.pop(JFK_val)
-    Wood_CQC_JFK.pop(JFK_val)
-    Cooked_CQC_JFK.pop(JFK_val)
-    Charcoal_CQC_JFK.pop(JFK_val)
-    Flour_In_CQC_JFK.pop(JFK_val)
-    Stir_CQC_JFK.pop(JFK_val)
-    Boil_CQC_JFK.pop(JFK_val)
-    CE_Time_CQC_JFK.pop(JFK_val)
+#for JFK_val in CQC_JFK_Non_Filter:
+#    Name_CQC_JFK.pop(JFK_val)
+#    Wood_CQC_JFK.pop(JFK_val)
+#    Cooked_CQC_JFK.pop(JFK_val)
+#    Charcoal_CQC_JFK.pop(JFK_val)
+#    Flour_In_CQC_JFK.pop(JFK_val)
+#    Stir_CQC_JFK.pop(JFK_val)
+#    Boil_CQC_JFK.pop(JFK_val)
+#    CE_Time_CQC_JFK.pop(JFK_val)
 
 
 ## Wood Savings
+
 Wood_CQC = list(Wood_CQC)
 Wood_CQC.remove(-1)
 
@@ -393,12 +426,14 @@ HH_3_TSF_Wood = []; HH_3_TSF_Char = []; HH_3_TSF_Cooked = []; HH_3_TSF_Boil = []
 HH_4_TSF_Wood = []; HH_4_TSF_Char = []; HH_4_TSF_Cooked = []; HH_4_TSF_Boil = []; HH_4_TSF_CE_Time = []
 HH_5_TSF_Wood = []; HH_5_TSF_Char = []; HH_5_TSF_Cooked = []; HH_5_TSF_Boil = []; HH_5_TSF_CE_Time = []
 HH_6_TSF_Wood = []; HH_6_TSF_Char = []; HH_6_TSF_Cooked = []; HH_6_TSF_Boil = []; HH_6_TSF_CE_Time = []
-print(Wood_TSF.iloc[8],type(Boil_TSF), Boil_TSF, type(CE_Time_TSF), type(Charcoal_TSF))
+print(Wood_TSF[8],type(Boil_TSF), Boil_TSF, type(CE_Time_TSF), type(Charcoal_TSF))
+
+print('boil and tfc ',Boil_TSF, type(Boil_TSF) )
 
 for tl, hh_TSF in enumerate(Name_TSF):
     HH_count = 0
     for letter in hh_TSF:
-        if HH_count == 2:
+        if HH_count == 2 and tl <= len(Boil_TSF)-1 and tl <= len(CE_Time_TSF)-1:
             print(letter, tl)
             TSF_Filter_HH.append('HH-'+letter)
             if letter == '1':
@@ -408,35 +443,34 @@ for tl, hh_TSF in enumerate(Name_TSF):
                 HH_1_TSF_Boil.append(Boil_TSF[tl])
                 HH_1_TSF_CE_Time.append(CE_Time_TSF[tl])
             elif letter == '2':
-                HH_2_TSF_Wood.append(Wood_TSF.iloc[tl])
-                HH_2_TSF_Char.append(Charcoal_TSF.iloc[tl])
-                HH_2_TSF_Cooked.append(Cooked_TSF.iloc[tl])
-                HH_2_TSF_Boil.append(Boil_TSF.iloc[tl])
-                HH_2_TSF_CE_Time.append(CE_Time_TSF.iloc[tl])
+                HH_2_TSF_Wood.append(Wood_TSF[tl])
+                HH_2_TSF_Char.append(Charcoal_TSF[tl])
+                HH_2_TSF_Cooked.append(Cooked_TSF[tl])
+                HH_2_TSF_Boil.append(Boil_TSF[tl])
+                HH_2_TSF_CE_Time.append(CE_Time_TSF[tl])
             elif letter == '3':
-                HH_3_TSF_Wood.append(Wood_TSF.iloc[tl])
-                HH_3_TSF_Char.append(Charcoal_TSF.iloc[tl])
-                HH_3_TSF_Cooked.append(Cooked_TSF.iloc[tl])
-                HH_3_TSF_Boil.append(Boil_TSF.iloc[tl])
-                HH_3_TSF_CE_Time.append(CE_Time_TSF.iloc[tl])
+                HH_3_TSF_Wood.append(Wood_TSF[tl])
+                HH_3_TSF_Char.append(Charcoal_TSF[tl])
+                HH_3_TSF_Cooked.append(Cooked_TSF[tl])
+                HH_3_TSF_Boil.append(Boil_TSF[tl])
+                HH_3_TSF_CE_Time.append(CE_Time_TSF[tl])
             elif letter == '4':
-                HH_4_TSF_Wood.append(Wood_TSF.iloc[tl])
-                HH_4_TSF_Char.append(Charcoal_TSF.iloc[tl])
-                HH_4_TSF_Cooked.append(Cooked_TSF.iloc[tl])
-                HH_4_TSF_Boil.append(Boil_TSF.iloc[tl])
-                HH_4_TSF_CE_Time.append(CE_Time_TSF.iloc[tl])
+                HH_4_TSF_Wood.append(Wood_TSF[tl])
+                HH_4_TSF_Char.append(Charcoal_TSF[tl])
+                HH_4_TSF_Cooked.append(Cooked_TSF[tl])
+                HH_4_TSF_Boil.append(Boil_TSF[tl])
+                HH_4_TSF_CE_Time.append(CE_Time_TSF[tl])
             elif letter == '5':
                 HH_5_TSF_Wood.append(Wood_TSF[tl])
                 HH_5_TSF_Char.append(Charcoal_TSF[tl])
-                HH_5_TSF_Cooked.append(Cooked_TSF[tl])
                 HH_5_TSF_Boil.append(Boil_TSF[tl])
                 HH_5_TSF_CE_Time.append(CE_Time_TSF[tl])
             elif letter == '6':
-                HH_6_TSF_Wood.append(Wood_TSF.iloc[tl])
-                HH_6_TSF_Char.append(Charcoal_TSF.iloc[tl])
-                HH_6_TSF_Cooked.append(Cooked_TSF.iloc[tl])
-                HH_6_TSF_Boil.append(Boil_TSF.iloc[tl])
-                HH_6_TSF_CE_Time.append(CE_Time_TSF.iloc[tl])
+                HH_6_TSF_Wood.append(Wood_TSF[tl])
+                HH_6_TSF_Char.append(Charcoal_TSF[tl])
+                HH_6_TSF_Cooked.append(Cooked_TSF[tl])
+                HH_6_TSF_Boil.append(Boil_TSF[tl])
+                HH_6_TSF_CE_Time.append(CE_Time_TSF[tl])
                 
         HH_count = HH_count + 1
 

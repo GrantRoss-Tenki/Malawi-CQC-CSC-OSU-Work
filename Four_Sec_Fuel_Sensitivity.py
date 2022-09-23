382407a0d1f182506d02cfbbe27be2ab86a7e37c
GrantRoss-Tenki <97212730+GrantRoss-Tenki@users.noreply.github.com>
2022-09-23 15:00:25 -0700

    Create Four_Sec_Fuel_Sensitivity.py
    New file to find sensistivity

---
+++ Four_Sec_Fuel_Sensitivity.py
@@ -0,0 +1,23 @@
+
+import itertools
+import os
+from types import prepare_class
+import pandas as pd
+import numpy as np
+import csv
+import glob
+from decimal import *
+from itertools import chain
+#import statistics as stat
+import datetime
+from datetime import datetime
+from itertools import islice, cycle
+from io import StringIO
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+
+Fueel = pd.read_csv("//depot.engr.oregonstate.edu/users/rossgra/Windows.Documents/My Documents/CSV_Tester/Fuel_sens_1.csv")

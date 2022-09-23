ca42e071fa103a0fbbde9b6adba6ac26f3658e88
GrantRoss-Tenki <97212730+GrantRoss-Tenki@users.noreply.github.com>
2022-09-23 15:41:22 -0700

    Update Four_Sec_Fuel_Sensitivity.py
    works, can share. threshold is 0.01 and window is set to 15 for a 15 log rate per minute

--- Four_Sec_Fuel_Sensitivity.py
+++ Four_Sec_Fuel_Sensitivity.py
@@ -18,6 +18,28 @@ import seaborn as sns
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
+import Functions_malawi
+
+read = (pd.read_csv("//depot.engr.oregonstate.edu/users/rossgra/Windows.Documents/My Documents/CSV_Tester/Fuel_sens_1.csv"))
+Fueel = read.iloc[:,0]
+print('-------', Fueel[1])
+#Threshold_array = np.arange(.0001, 0.06,0.0001)
+Threshold_array = np.arange(1, 30,0.1)
+
+KG_Removed = []
+for thresh in Threshold_array:
+    KG_burned_1, KG_1_mean = Functions_malawi.FUEL_REMOVAL(Fueel, 0.01, 15, True, thresh)
+    
+    #eliminate_repeat = Functions_malawi.Remove_Repeated_Values(KG_burned_1)
+    KG_Removed.append(sum(list(set(KG_burned_1))))
 
 
-Fueel = pd.read_csv("//depot.engr.oregonstate.edu/users/rossgra/Windows.Documents/My Documents/CSV_Tester/Fuel_sens_1.csv")
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=Threshold_array, y=KG_Removed,
+                mode='lines+markers'))
+                    
+fig.update_layout(title=" Sensitivity for Fuel Threshold",
+                   xaxis_title='Threshold',
+                   yaxis_title='Fuel Removed')
+
+fig.show()

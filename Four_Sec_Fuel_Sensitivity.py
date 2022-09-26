75af90c0a7e418fce0c559eb767038e4bbadd734
GrantRoss-Tenki <97212730+GrantRoss-Tenki@users.noreply.github.com>
2022-09-26 09:55:09 -0700

    Update Four_Sec_Fuel_Sensitivity.py
    Made it to run on laptop

--- Four_Sec_Fuel_Sensitivity.py
+++ Four_Sec_Fuel_Sensitivity.py
@@ -20,15 +20,16 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import Functions_malawi
 
-read = (pd.read_csv("//depot.engr.oregonstate.edu/users/rossgra/Windows.Documents/My Documents/CSV_Tester/Fuel_sens_1.csv"))
+#read = (pd.read_csv("//depot.engr.oregonstate.edu/users/rossgra/Windows.Documents/My Documents/CSV_Tester/Fuel_sens_1.csv"))
+read = (pd.read_csv("C:/Users/gvros/Documents/CSV_FUEL_tester.csv"))
 Fueel = read.iloc[:,0]
 print('-------', Fueel[1])
-#Threshold_array = np.arange(.0001, 0.06,0.0001)
-Threshold_array = np.arange(1, 30,0.1)
+Threshold_array = np.arange(.0001, 0.06,0.0001)
+#Threshold_array = np.arange(1, 30,0.1)
 
 KG_Removed = []
 for thresh in Threshold_array:
-    KG_burned_1, KG_1_mean = Functions_malawi.FUEL_REMOVAL(Fueel, 0.01, 15, True, thresh)
+    KG_burned_1, KG_1_mean = Functions_malawi.FUEL_REMOVAL(Fueel, thresh, 15, True, 15)
     
     #eliminate_repeat = Functions_malawi.Remove_Repeated_Values(KG_burned_1)
     KG_Removed.append(sum(list(set(KG_burned_1))))

4858244810f97a42406b084cd60a4ef6023910fb
GrantRoss-Tenki <97212730+GrantRoss-Tenki@users.noreply.github.com>
2023-03-21 11:48:34 -0700

    Create Fire_finder_function.py

---
+++ Fire_finder_function.py
@@ -0,0 +1,15 @@
+import itertools
+import os
+#from typing_extensions import clear_overloads
+import pandas as pd
+import numpy as np
+import csv
+import glob
+from decimal import *
+from itertools import chain
+import statistics as stat
+import datetime
+from io import StringIO
+import matplotlib.pyplot as plt
+# Fire Finder Algorithm
+
